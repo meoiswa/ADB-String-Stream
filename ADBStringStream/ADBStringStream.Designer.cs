8257e47f3b41fc23d4222d82fe3a8fc9e522a0a2
Alejandro Font <meoiswa@gmail.com>
2014-08-13 18:30:07 -0430

    Initial Commit

---
+++ ADBStringStream/ADBStringStream.Designer.cs
@@ -0,0 +1,113 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+namespace ADBStringStream
+{
+    partial class ADBStringStream
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxString = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(197, 10);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(72, 12);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(119, 20);
+            this.textBoxIP.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Device IP";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Enabled = false;
+            this.buttonSend.Location = new System.Drawing.Point(12, 67);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(260, 23);
+            this.buttonSend.TabIndex = 5;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxString
+            // 
+            this.textBoxString.Enabled = false;
+            this.textBoxString.Location = new System.Drawing.Point(12, 41);
+            this.textBoxString.Name = "textBoxString";
+            this.textBoxString.Size = new System.Drawing.Size(260, 20);
+            this.textBoxString.TabIndex = 6;
+            // 
+            // ADBStringStream
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 99);
+            this.Controls.Add(this.textBoxString);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "ADBStringStream";
+            this.Text = "ADB String Stream";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button buttonConnect;
+        private TextBox textBoxIP;
+        private Label label1;
+        private Button buttonSend;
+        private TextBox textBoxString;
+
+    }
+}
+
---
+++ ADBStringStream/ADBStringStream.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADBStringStream
+{
+    public partial class ADBStringStream : Form
+    {
+        bool connection = false;
+
+        public ADBStringStream()
+        {
+            InitializeComponent();
+
+        }
+
+        public void connected()
+        {
+            this.connection = true;
+
+            buttonConnect.Text = "Disconnect";
+            buttonConnect.Enabled = true;
+            textBoxIP.Enabled = false;
+
+            buttonSend.Enabled = true;
+            textBoxString.Enabled = true;
+        }
+
+        public void disconnected()
+        {
+            this.connection = false;
+
+            buttonConnect.Text = "Connect";
+            buttonConnect.Enabled = true;
+            textBoxIP.Enabled = true;
+
+            buttonSend.Enabled = false;
+            textBoxString.Enabled = false;
+        }
+
+        public void disableButtons()
+        {
+            buttonSend.Enabled = false;
+            buttonConnect.Enabled = false;
+            textBoxIP.Enabled = false;
+            textBoxString.Enabled = false;
+        }
+
+        public bool testForDevice()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = "adb.exe";
+            startInfo.Arguments = "get-state";
+            startInfo.RedirectStandardOutput = true;
+
+            try
+            {
+                // Start the process with the info we specified.
+                // Call WaitForExit and then the using statement will close.
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    exeProcess.WaitForExit();
+                    var output = exeProcess.StandardOutput.ReadToEnd();
+
+                    Debug.WriteLine(output);
+
+                    if ( output.Contains("device"))
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            disableButtons();
+            if (connection)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.CreateNoWindow = true;
+                startInfo.UseShellExecute = false;
+                startInfo.FileName = "adb.exe";
+                startInfo.Arguments = "disconnect ";
+                startInfo.RedirectStandardOutput = true;
+
+                try
+                {
+                    // Start the process with the info we specified.
+                    // Call WaitForExit and then the using statement will close.
+                    using (Process exeProcess = Process.Start(startInfo))
+                    {
+                        exeProcess.WaitForExit();
+                        var output = exeProcess.StandardOutput.ReadToEnd();
+
+                        Debug.WriteLine(output);
+                    }
+                }
+                catch
+                {
+                    // Log error.
+                }
+                disconnected();
+                return;
+            }
+            if (textBoxIP.Text.Length > 0)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.CreateNoWindow = true;
+                startInfo.UseShellExecute = false;
+                startInfo.FileName = "adb.exe";
+                startInfo.Arguments = "connect " + textBoxIP.Text;
+                startInfo.RedirectStandardOutput = true;
+
+                try
+                {
+                    // Start the process with the info we specified.
+                    // Call WaitForExit and then the using statement will close.
+                    using (Process exeProcess = Process.Start(startInfo))
+                    {
+                        exeProcess.WaitForExit();
+                        var output = exeProcess.StandardOutput.ReadToEnd();
+
+                        Debug.WriteLine(output);
+
+                        if (output.Contains("connected"))
+                        {
+                            connected();
+                        }
+                        else
+                        {
+                            disconnected();
+                        }
+                    }
+                }
+                catch
+                {
+                    // Log error.
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            if (testForDevice())
+            {
+                connected();
+            }
+            else
+            {
+                disconnected();
+            }
+
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            disableButtons();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = "adb.exe";
+            startInfo.Arguments = @"shell input text """ + textBoxString.Text.Replace(" ","%s") + @"""";
+            Console.WriteLine(startInfo.Arguments);
+            
+            startInfo.RedirectStandardOutput = true;
+
+            try
+            {
+                // Start the process with the info we specified.
+                // Call WaitForExit and then the using statement will close.
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    exeProcess.WaitForExit();
+                    var output = exeProcess.StandardOutput.ReadToEnd();
+                    textBoxString.Text = "";
+                    Debug.WriteLine(output);
+                }
+            }
+            catch
+            {
+                // Log error.
+            }
+
+            startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = "adb.exe";
+            startInfo.Arguments = "shell input keyevent 66";
+            startInfo.RedirectStandardOutput = true;
+
+            try
+            {
+                // Start the process with the info we specified.
+                // Call WaitForExit and then the using statement will close.
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    exeProcess.WaitForExit();
+                    var output = exeProcess.StandardOutput.ReadToEnd();
+
+                    Debug.WriteLine(output);
+                }
+            }
+            catch
+            {
+                // Log error.
+            }
+            connected();
+        }
+
+    }
+}
